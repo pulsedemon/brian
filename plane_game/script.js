184cac22842727470021351d4243f74eca40be6c
punkcow <briansquashic@gmail.com>
2015-02-20 14:34:20 -0500

    code to keep jet inside browser added

--- plane_game/script.js
+++ plane_game/script.js
@@ -25,12 +25,12 @@ BGposX = 0;
 
 
 canvas = document.getElementById('background-canvas');
-ctx = canvas.getContext('2d');
-ctx.canvas.width  = window.innerWidth;
-ctx.canvas.height = window.innerHeight;
+	ctx = canvas.getContext('2d');
+	ctx.canvas.width  = window.innerWidth;
+	ctx.canvas.height = window.innerHeight;
 
 (function draw_background() {
-	var img, W, H;
+	var W, H, img;
 	W = canvas.width;
 	H = canvas.height;
 	img = new Image();
@@ -38,10 +38,10 @@ ctx.canvas.height = window.innerHeight;
 
 	window.requestAnimationFrame(draw_background);
 	ctx.clearRect(0, 0, W, H);
+	ctx.fillRect(0, 0, W, H);
 	ctx.drawImage(img, BGposX, 0);
 	ctx.drawImage(img, img.width-Math.abs(BGposX), 0);
-
-	if (Math.abs(BGposX) > img.width) {
+	if (Math.abs(BGposX) >= img.width) {
     	BGposX = 0;
 	}
 	
@@ -59,26 +59,47 @@ jet_id.onmousedown = function() {
 	grab(this);
 	return false;
 }
-
 function grab(jetContainer) {
 	selected = jetContainer;
 	x_element = x_pos - selected.offsetLeft;
 	y_element = y_pos - selected.offsetTop;
 }
-
 function random_fire() {
 	var fires = ['fire1.png', 'fire2.png'];
-	return fires[Math.floor(Math.random() * fires.length)];
+	return fires[Math.floor(Math.random() * fires.length)];5
 }
 
 fire = document.getElementById('fire');
 
 function move_plane(jetContainer) {
+	var jet_positionX, jet_positionY;
 	x_pos = jetContainer.pageX || window.event.clientX;
 	y_pos = jetContainer.pageY || window.event.clientY;
 	if (selected !== null) {
-		selected.style.left = (x_pos - x_element) + 'px';
-		selected.style.top = (y_pos - y_element) + 'px';
+
+		// this code prevents jetContainer from being moved out of view in the browser.
+		jet_positionX = x_pos - x_element;
+		if(jet_positionX >= ctx.canvas.width - jet_id.offsetWidth) {
+			selected.style.left = ctx.canvas.width - jet_id.offsetWidth + 'px';
+		}
+		else if(jet_positionX <= 0) {
+			selected.style.left = 0 + 'px';
+		}
+		else{
+			selected.style.left = jet_positionX + 'px';
+		}
+
+		jet_positionY = y_pos - y_element;
+		if(jet_positionY >= ctx.canvas.height - jet_id.offsetHeight) {
+			selected.style.top = ctx.canvas.height - jet_id.offsetHeight + 'px';
+		}
+		else if(jet_positionY <= 0) {
+			selected.style.top = 0 + 'px';
+		}
+		else {
+		selected.style.top = jet_positionY + 'px';
+		}
+
 		fire.style.cssText += 'background: url(' + random_fire() + '), url("fire3-tiny.png");';
 	}	
 }
